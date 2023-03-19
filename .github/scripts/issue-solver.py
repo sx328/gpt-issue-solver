93d0a7c8b0f343fa6d9436e61cf75dc8bc0d8226
Jeffrey Annaraj <jannaraj@baffled.dev>
2023-03-19 18:24:54 -0500

    initial commit

---
+++ .github/scripts/issue-solver.py
@@ -0,0 +1,28 @@
+import os
+import openai
+from github import Github
+
+def get_gpt4_response(prompt, max_tokens=50):
+    response = openai.Completion.create(
+        engine="gpt-4",
+        prompt=prompt,
+        max_tokens=max_tokens,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    return response.choices[0].text.strip()
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
+github_token = os.environ["GITHUB_TOKEN"]
+
+github_client = Github(github_token)
+repo = github_client.get_repo(os.environ["GITHUB_REPOSITORY"])
+
+issue_number = int(os.environ["ISSUE_NUMBER"])
+issue = repo.get_issue(number=issue_number)
+
+prompt = f"Help me solve this GitHub issue:\nTitle: {issue.title}\nDescription: {issue.body}\n"
+solution = get_gpt4_response(prompt)
+issue.create_comment(solution)
+print(f"Generated solution for issue {issue.number}: {solution}\n")
---
+++ README.md
@@ -0,0 +1,3 @@
+# gpt-issue-solver
+
+This project is a simple tool to help you solve issues with the [GPT-4](https://openai.com/product/gpt-4) API. It is currently a experimental project, and will attempt to solve Github issues when added.
