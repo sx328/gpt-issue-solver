fe1fd7909ad3242cfdb6e4d53c790688f2b0f439
Jeffrey Annaraj <jannaraj@baffled.dev>
2023-03-19 18:33:27 -0500

    replaced env vars due to conflict

--- .github/scripts/issue-solver.py
+++ .github/scripts/issue-solver.py
@@ -2,6 +2,7 @@ import os
 import openai
 from github import Github
 
+
 def get_gpt4_response(prompt, max_tokens=50):
     response = openai.Completion.create(
         engine="gpt-4",
@@ -13,8 +14,9 @@ def get_gpt4_response(prompt, max_tokens=50):
     )
     return response.choices[0].text.strip()
 
+
 openai.api_key = os.environ["OPENAI_API_KEY"]
-github_token = os.environ["GITHUB_TOKEN"]
+github_token = os.environ["ACTION_KEY"]
 
 github_client = Github(github_token)
 repo = github_client.get_repo(os.environ["GITHUB_REPOSITORY"])
